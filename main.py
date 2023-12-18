5801d53f0111868d21ae774402fcdf22295b1775
oneloveoneheart <gahetaflorent@gmail.com>
2023-12-18 12:48:41 +0100

    I set up the snake screen game

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import Screen, Turtle
+
+
+game_screen = Screen()
+game_screen.setup(width=600, height=600)
+game_screen.bgcolor("black")
+game_screen.title("Starving mamba")
+
+
+game_screen.exitonclick()
