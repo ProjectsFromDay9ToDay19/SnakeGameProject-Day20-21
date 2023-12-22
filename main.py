efcb84474ded87c2b596b6e78eebee8616133941
oneloveoneheart <gahetaflorent@gmail.com>
2023-12-22 23:05:02 +0100

    I refactored the code to create a snake file apart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import turtle
 from turtle import Screen, Turtle
+from snake import Snake
 
 # Setting up the screen
 game_screen = Screen()
@@ -9,27 +10,13 @@ game_screen.bgcolor("black")
 game_screen.title("Starving mamba")
 game_screen.tracer(0)
 
-# creating the snake body
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.penup()
-    new_segment.color("white")
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     game_screen.update()
     time.sleep(0.2)
-
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-
-    segments[0].forward(20)
+    snake.move()
 
 # keeping the screen on
 game_screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+
+class Snake:
+
+    def __init__(self):
+        self.snake_body = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.penup()
+            new_segment.color("white")
+            new_segment.goto(position)
+            self.snake_body.append(new_segment)
+
+    def move(self):
+        for segment_index in range(len(self.snake_body) - 1, 0, -1):
+            new_x = self.snake_body[segment_index - 1].xcor()
+            new_y = self.snake_body[segment_index - 1].ycor()
+            self.snake_body[segment_index].goto(new_x, new_y)
+
+        self.snake_body[0].forward(MOVE_DISTANCE)
