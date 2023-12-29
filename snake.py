5c9a7f97ca81cf6f114dc445e73a5c1ff49613c2
oneloveoneheart <gahetaflorent@gmail.com>
2023-12-29 20:38:00 +0100

    I updated the game on the level of one i've already bulit

--- snake.py
+++ snake.py
@@ -52,3 +52,21 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+
+    def reset(self):
+        for segment in self.snake_body:
+            segment.goto(1500, 1500)
+        self.snake_body.clear()
+        self.create_snake()
+        self.head = self.snake_body[0]
+
+    """def increase_speed(self):
+        # self.speed += 5
+        for segment in self.segments:
+            segment.speed(self.speed)"""
+
+    """def print_current_speed(self):
+        self.segments[0].goto(x=-590, y=350)
+        self.clear()
+        self.write(arg=f"Current Speed : {self.speed}", align=ALIGNMENT, font=FONT)
+"""
